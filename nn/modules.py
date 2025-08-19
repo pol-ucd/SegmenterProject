23ec99f3e500a80813379647c10831f342d11e02
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-08-19 20:03:20 +0100

    Cleaning up

--- nn/modules.py
+++ nn/modules.py
@@ -29,6 +29,7 @@ class HybridLoss(nn.Module):
 
 def dice_loss(pred, target, epsilon=1e-6):
     pred = torch.softmax(pred, dim=1)  # [B, C, H, W]
+    print(target.shape, target.dtype)
     target_onehot = F.one_hot(target, num_classes=pred.shape[1]).permute(0, 3, 1, 2).float()
     intersection = (pred * target_onehot).sum(dim=(2, 3))
     union = pred.sum(dim=(2, 3)) + target_onehot.sum(dim=(2, 3))
