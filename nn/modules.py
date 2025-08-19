166e3b91aa04b1f1054b7c423290b63a83a8e7a9
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-08-19 17:18:55 +0100

    Cleaning up

--- nn/modules.py
+++ nn/modules.py
@@ -26,18 +26,18 @@ class CombinedLoss(nn.Module):
         return self._do_calculation(pred, target)
 
     def _do_calculation(self, pred, target):
-        print(target.unique())
+        # print(target.unique())
         bce = self.bce(pred, target.unsqueeze(1).float())
-        print("BCEwithLogits ", bce)
+        # print("BCEwithLogits ", bce)
         pred = pred.transpose(3, 1)
         tversky = self.tversky(pred, target.float())
-        print("Tversky ", tversky)
+        # print("Tversky ", tversky)
         focal = self.focal(pred, target.float())
-        print("FocalLoss ", focal)
+        # print("FocalLoss ", focal)
         dice = self.dice(pred, target.float())
-        print("Dice Loss ", dice)
+        # print("Dice Loss ", dice)
         jaccard = self.iou(pred, target.float())
-        print("Jaccard Loss ", jaccard)
+        # print("Jaccard Loss ", jaccard)
         return (self.weights['bce'] * bce + self.weights['tversky'] * tversky
                 + self.weights['focal'] * focal + self.weights['dice'] * dice
                 + self.weights['jaccard'] * jaccard)
