2adaf38a277214be7dafb4bc72d1b3a590ad82ff
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-08-22 19:41:29 +0100

    Cleaning up

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 IMAGE_PATHS = [
-    # "data/Classica/images",
+    "data/Classica/images",
     "data/Polyp Segmentation/train",
-    "data/Polyp Segmentation/valid"]
+    "data/Polyp Segmentation/valid",
+    "data/Kvasir-SEG/images"]
 
 MASK_PATHS = [
-    # "data/Classica/masks",
+    "data/Classica/masks",
     "data/Polyp Segmentation/train_masks",
-    "data/Polyp Segmentation/valid_masks"]
+    "data/Polyp Segmentation/valid_masks",
+    "data/Kvasir-SEG/masks"]
 
 FILE_TYPES = ["*.jpg", "*.png", "*.jpeg"]
 
