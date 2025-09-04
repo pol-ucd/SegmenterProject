4a76a3a4bfd202cef9fe12005fc2964401773252
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-09-04 20:14:26 +0100

    Cleaning up

--- train_augur_hdf5.py
+++ train_augur_hdf5.py
@@ -66,7 +66,7 @@ def main():
     n_epochs = run_params['n_epochs']
 
     """ Optimiser settings """
-    opt_params = params['optimizer_settings']
+    opt_params = params['optimizer']
     learning_rate = opt_params['learning_rate']
     l2_decay_penalty = opt_params['l2_decay_penalty']
 
