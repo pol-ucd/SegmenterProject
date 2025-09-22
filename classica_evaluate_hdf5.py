f5729204a37e89a218edb8a7056ada4563fce0b4
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-09-22 10:37:11 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- classica_evaluate_hdf5.py
+++ classica_evaluate_hdf5.py
@@ -24,8 +24,9 @@ from segmenter.torch_utils import RunManager, CheckpointManager
 def check_scores(metric:dict[str,list])-> bool:
     all_lens = np.array([len(v) for v in metric.values()])
     base_len = all_lens[0]
-    for k,v in metric.items():
-        print(f"{k}: {len(v)}")
+    if not np.all(all_lens == base_len)
+        for k,v in metric.items():
+            print(f"{k}: {len(v)}")
     return np.all(all_lens == base_len)
 
 
