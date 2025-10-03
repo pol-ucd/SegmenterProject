13c358419b72ae11f901c22bb6f6328570ee00c1
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-03 10:56:41 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- segmenter/utils/data.py
+++ segmenter/utils/data.py
@@ -205,7 +205,7 @@ class HDF5DatasetOptimized(Dataset):
         results = {}
         for k, v in batch_data.items():
             # Use torch.as_tensor() or torch.from_numpy() for zero-copy conversion
-            tensor = torch.as_tensor(v).float()
+            tensor = torch.as_tensor(v).float().permute(2, 0, 1)
             if self.transform:
                 results[k] = self.transform(tensor)
             else:
