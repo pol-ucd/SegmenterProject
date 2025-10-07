42c888058f190a97c172ab8e70dc001520cf26a2
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-07 19:19:29 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_moco2.py
+++ msn_moco2.py
@@ -69,8 +69,8 @@ def pretrain_step(model: MoCoSiameseNetwork,
                                     dtype=torch.float,
                                     enabled=(scaler is not None)):
                 online_emb, target_emb = model(x, epoch=epoch, batch_index=batch_idx)
-                online_emb = online_emb.to(dtype=torch.float32)
-                target_emb = target_emb.to(dtype=torch.float32)
+                online_emb = online_emb.to(dtype=torch.float32).to(device)
+                target_emb = target_emb.to(dtype=torch.float32).to(device)
 
                 loss = loss_fn(online_emb, target_emb)
 
