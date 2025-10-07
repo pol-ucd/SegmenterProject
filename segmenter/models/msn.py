33ba5b93b51c64858728bbef5ce44a598aed0fe1
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-07 20:03:09 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- segmenter/models/msn.py
+++ segmenter/models/msn.py
@@ -389,7 +389,7 @@ class SegFormerFeatureWrapper(nn.Module):
 
     @torch.no_grad()
     def _extract_encoder_stage(self, encoder_module, x: torch.Tensor):
-        outs = encoder_module(x.float()).to(self._device)
+        outs = encoder_module(x)
         feat = outs[self.stage_idx] if isinstance(outs, (list, tuple)) else outs
         return feat.last_hidden_state.to(self._device)  # (B, D, H', W')
 
