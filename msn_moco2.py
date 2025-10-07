9377667c682fc7207072c36f4ef85875c1088d5f
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-07 16:16:55 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_moco2.py
+++ msn_moco2.py
@@ -65,7 +65,8 @@ def pretrain_step(model: MoCoSiameseNetwork,
 
             optimizer.zero_grad()
             with torch.amp.autocast(device_type=device.type,
-                                    enabled=(scaler is not None)):
+                                    dtype=torch.float16):
+                                    # enabled=(scaler is not None)):
                 online_emb, target_emb = model(x, epoch=epoch, batch_index=batch_idx)
                 online_emb = online_emb.to(dtype=torch.float32)
                 target_emb = target_emb.to(dtype=torch.float32)
