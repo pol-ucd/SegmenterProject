5e887e39b2d95b29ec53d02e545eb86e5be19d67
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-08 14:37:49 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_moco2.py
+++ msn_moco2.py
@@ -61,10 +61,10 @@ def pretrain_step(model: MoCoSiameseNetwork,
         for batch_idx, batch_images in enumerate(dataloader):
             x = batch_images["images"].to(device)
             optimizer.zero_grad()
-            # with torch.amp.autocast(device_type=get_default_device_type(),
-            #                         dtype=torch.float16,
-            #                         enabled=(scaler is not None)):
-            with torch.amp.autocast():
+
+            with torch.amp.autocast(device_type=get_default_device_type(),
+                                    dtype=torch.float16,
+                                    enabled=(scaler is not None)):
                 online_emb, target_emb = model(x, epoch=epoch, batch_index=batch_idx)
                 online_emb = online_emb.to(dtype=torch.float32).to(device)
                 target_emb = target_emb.to(dtype=torch.float32).to(device)
