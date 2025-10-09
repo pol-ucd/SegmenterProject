2b2f030117366efb1004453af91f790483da5aa8
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-09 18:16:09 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_moco2.py
+++ msn_moco2.py
@@ -243,10 +243,10 @@ def main():
     siamese_model = MoCoSiameseNetwork(pretrained_model=backbone_model,
                                        momentum=0.9).to(device)
 
-    pretrain_loss_fn = MSNLoss(temperature=0.9, center_momentum=0.001)
+    # pretrain_loss_fn = MSNLoss(temperature=0.9, center_momentum=0.001)
     # pretrain_loss_fn = NTXEntLoss(temperature=0.9, eps=1e-9)
     # pretrain_loss_fn = ContrastiveLoss(temperature=0.1, eps=1e-6)
-
+    pretrain_loss_fn = MSELoss()
     # Use a large LR for pre-training (standard for self-supervised learning)
     pretrain_optimizer = torch.optim.AdamW(siamese_model.parameters(),
                                            lr=learning_rate,
