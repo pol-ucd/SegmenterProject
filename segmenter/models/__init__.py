1abc5e5c0b0b40bef5bafd6967a65b47489a6b74
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-13 09:54:49 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- segmenter/models/__init__.py
+++ segmenter/models/__init__.py
@@ -2,9 +2,9 @@ __author__ = "Pol Mac Aonghusa"
 __email__ = "polmacaonghusa@gmail.com"
 
 from .base import MedianPool2d, SegformerModelError, SegformerBackbone, SupervisedSegFormer
-from .msn import (SimSiamSegFormer, MoCoSiameseNetwork, SimCLRSegFormer,
-                  SurgicalMaskComposer, MaskedTiledViewGenerator, SegFormerMSNWithMomentum)
+from .msn import (SimSiamSegFormer, MoCoSiameseNetwork, SimCLRSegFormer, SegFormerFeatureWrapper,
+                  SurgicalMaskComposer, MaskedTiledViewGenerator)
 
-__all__ = ['MedianPool2d', 'SegformerModelError', 'SegformerBackbone',
+__all__ = ['MedianPool2d', 'SegformerModelError', 'SegformerBackbone', 'SegFormerFeatureWrapper',
            'SurgicalMaskComposer', 'MaskedTiledViewGenerator', 'SegformerModelError',
            'SimCLRSegFormer', 'MoCoSiameseNetwork', 'SupervisedSegFormer',]
