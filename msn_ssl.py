31ddd473cae5ddfe254563cafe0546a151fdcd93
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-21 12:15:10 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_ssl.py
+++ msn_ssl.py
@@ -310,13 +310,14 @@ def show_batch(loader: DataLoader, n_batches: int = 1) -> None:
 def main(params: Dict[str, Any]):
     logger = logging.getLogger(__name__)
 
-    prefix = params.get('prefix', 'msn_moco_2')
+    prefix = params.get('prefix', 'msn_moco')
     if prefix == '':
-        prefix = 'msn_moco_2'
+        prefix = 'msn_moco'
 
     image_size = (512, 512)
-    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    scaler = torch.amp.GradScaler() if torch.cuda.is_available() else None
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # scaler = torch.amp.GradScaler() if torch.cuda.is_available() else None
+    scaler = None
 
     ds = HDF5DatasetOptimized(hdf5_path=params['dataset'],
                               transform=SSLTransformPipeline(size=image_size))
