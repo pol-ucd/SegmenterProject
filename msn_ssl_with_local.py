591ef7ba9d37f165c0f2579708cd5966152bba03
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-23 15:38:36 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_ssl_with_local.py
+++ msn_ssl_with_local.py
@@ -418,7 +418,7 @@ def main(params: Dict[str, Any]):
 
     image_size = (512, 512)
     if torch.cuda.is_available():
-        device = torch.device('cuda:0')
+        device = torch.device('cuda')
         device_type = 'cuda'
         scaler = torch.amp.GradScaler()
     else:
