0a15878dafda6379d0ddfbecb4a5b9b0a349866f
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-23 13:17:34 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_ssl_with_local.py
+++ msn_ssl_with_local.py
@@ -511,7 +511,7 @@ def main(params: Dict[str, Any]):
 
                 with torch.no_grad():
                     z_target_upscaled = teacher_model(z_target)
-                    z_local_upscale = z_target_upscaled.repeat(n_local_repeats, 1, 1, 1).to(device)
+                    # z_local_upscale = z_target_upscaled.repeat(n_local_repeats, 1, 1, 1).to(device)
 
                 # loss = (0.5*criterion(x_anchor_upscaled, z_target_upscaled, pixel_mask) +
                 #         0.5*criterion(local_anchors_upscaled, z_local_upscale, local_pixel_mask))
