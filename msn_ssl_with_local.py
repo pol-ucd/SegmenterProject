298a9dc6c1a779b94ff797f089a50be13244c807
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-23 12:09:37 +0100

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- msn_ssl_with_local.py
+++ msn_ssl_with_local.py
@@ -481,11 +481,13 @@ def main(params: Dict[str, Any]):
             x_anchor = batch['anchors'].to(device)
             x_anchor.requires_grad = True
             pixel_mask = torch.logical_not(mask_generator.generate_pixel_mask(x_anchor).bool()).to(device)
+            x_anchor_masked = x_anchor*pixel_mask.float()
 
             local_anchors = batch['local_anchors']
             local_anchors.requires_grad = True
             n_local_repeats = int(local_anchors.shape[0] / pixel_mask.shape[0])
             local_pixel_mask = pixel_mask.repeat(n_local_repeats, 1, 1, 1).to(device)
+            local_anchors_masked = local_anchors * local_pixel_mask.float()
 
             """ Target images """
             z_target = batch['targets'].to(device)
@@ -498,8 +500,8 @@ def main(params: Dict[str, Any]):
 
             with autocast(device_type=device_type):
 
-                x_anchor_upscaled = student_model(x_anchor*pixel_mask)
-                local_anchors_upscaled = student_model(local_anchors*local_pixel_mask)
+                x_anchor_upscaled = student_model(x_anchor_masked)
+                local_anchors_upscaled = student_model(local_anchors_masked)
 
                 with torch.no_grad():
                     z_target_upscaled = teacher_model(z_target)
