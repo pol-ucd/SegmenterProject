2753bf651d0e42788ffff315c7b95e12a32e7a7a
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-10-30 21:05:57 +0000

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- attention_mim.py
+++ attention_mim.py
@@ -53,10 +53,7 @@ class MIMSegformerReconstructionHead(nn.Module):
     """
     def __init__(self, config):
         super().__init__()
-        h_patch = config.image_size // config.strides[0]
-        w_patch = h_patch
-        n_features = sum(config.hidden_sizes)
-        decoder_head_in = n_features*h_patch*w_patch
+
         mlps = []
         for i in range(config.num_encoder_blocks):
             mlp = MIMUpscalerMLP(input_dim=config.hidden_sizes[i], output_dim=config.decoder_hidden_size)
@@ -78,6 +75,10 @@ class MIMSegformerReconstructionHead(nn.Module):
             Use an image reconstruction head instead of the usual classifier so 
             that we can compare generated images to calculate loss
         """
+        h_patch = config.image_size // config.strides[0]
+        w_patch = h_patch
+
+        decoder_head_in = h_patch * w_patch * config.decoder_hidden_size
         print("decoder_head_in: ", decoder_head_in, 3*config.image_size*config.image_size)
         self.reconstruction_head = nn.Linear(decoder_head_in,
                                     3*config.image_size*config.image_size)
