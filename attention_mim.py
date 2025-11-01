c8c1e234d3ae9aae9e7ba1b28d22ff5f87a9043b
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-11-01 22:40:47 +0000

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- attention_mim.py
+++ attention_mim.py
@@ -149,7 +149,7 @@ class AttentionMaskingMIM(nn.Module):
 
         mask = torch.ones_like(x).long()
         attention_mask = 1.0 - self.generate_attention_mask(attn_map).requires_grad_(True)
-        attention_mask = F.interpolate(mask,
+        attention_mask = F.interpolate(attention_mask,
                              size=(H, W), mode='nearest').unsqueeze(1)
         attention_mask = (attention_mask > 0.5).long()
 
