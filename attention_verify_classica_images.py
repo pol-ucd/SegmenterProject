43cf20ded06c64c7444fee162813227fe282cfd1
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-11-05 10:01:35 +0000

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- attention_verify_classica_images.py
+++ attention_verify_classica_images.py
@@ -338,7 +338,7 @@ def main(params: dict[str, Any]):
                 assert seg_map.shape == masks.shape, (f"Size mismatch between "
                                                       f"generated mask: {seg_map.shape}, "
                                                       f"and target mask: {masks.shape}")
-                print("seg_map min/max range:", np.min(seg_map), np.max(seg_map))
+                print("seg_map min/max range:", seg_map.min(), seg_map.max())
                 loss_train = 0.5*loss_fn1(seg_map, masks) + 0.5*loss_fn2(seg_map, masks)
 
             if scaler is not None:
