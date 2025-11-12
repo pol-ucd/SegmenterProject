16ce41786c46a20a0ba1f1964246bc70d76dd269
Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>
2025-11-12 16:48:08 +0000

    Cleaning up
    Signed-off-by: Pol Mac Aonghusa <74455977+pol-maca@users.noreply.github.com>

--- extract_masks_images_hdf5.py
+++ extract_masks_images_hdf5.py
@@ -2,6 +2,7 @@ import os
 
 import h5py
 from PIL import Image
+from tqdm import tqdm
 
 hdf5_file = "/home/pol/segmenter/data/dresden_preprocessed.h5"
 base_dir = "/home/pol/dresden_data"
@@ -16,21 +17,14 @@ if __name__ == "__main__":
 
     hdf5_file = h5py.File(hdf5_file, 'r', swmr=True)
 
-    print(hdf5_file.keys())
-
     images = hdf5_file['images']
     masks = hdf5_file['masks']
-    orig_sizes = hdf5_file['orig_size']
-    orig_names = hdf5_file['original_name']
 
+    for idx, (image, mask) in enumerate(tqdm(zip(images, masks))):
+        image_pil = Image.fromarray(image).convert('RGB')
+        mask_pil = Image.fromarray(mask[1] * 255).convert('L')
 
-    for image, mask, orig_size, orig_name in zip(images, masks, orig_sizes, orig_names):
-        image_pil = Image.fromarray(image).convert('RGB').resize(orig_size,
-                                                                 Image.Resampling.LANCZOS)
-        mask_pil = Image.fromarray(mask[1]*255).convert('L').resize(orig_size,
-                                                                    Image.Resampling.NEAREST)
+        name = f"Dresden_{idx:06d}.jpg"
 
-        image_pil.save(os.path.join(outdir_images, orig_name.decode('utf-8')))
-        mask_pil.save(os.path.join(outdir_masks, orig_name.decode('utf-8')))
-
-    print(hdf5_file.keys())
+        image_pil.save(os.path.join(outdir_images, name))
+        mask_pil.save(os.path.join(outdir_masks, name))
